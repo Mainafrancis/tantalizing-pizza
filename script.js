1b5030c601b8f948dd16de685ee4f11e65664d83
Mainafrancis <francis.maina@student.moringaschool.com>
2022-03-19 19:15:44 +0300

    config js

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
     $(".pick").click(function(event){
         alert('Order successful!');
         event.preventDefault();
+    });
+    $("#delivery").hide();
+    $(".make").click(function(){
+        $('#delivery').toggle();
     })
 })
