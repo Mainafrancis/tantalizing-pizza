58febe9c6d6d7e8dc0e6af3571e5198c5cfdd83a
Mainafrancis <francis.maina@student.moringaschool.com>
2022-03-19 19:27:06 +0300

    push js

--- script.js
+++ script.js
@@ -11,5 +11,7 @@ $(document).ready(function(){
         var person = document.getElementById("first-name").value;
         var hood = document.getElementById("hood").value;
         var house = document.getElementById("hseNo").value;
+
+        alert('Dear' + "" + ""+ 'we have received your message.' + 'Delivery will be done in 20 minutes to House Number ' + "" + house + "," + 'on' + '' + hood + "hood." + 'Thanks for ordering.');
     })
 })
