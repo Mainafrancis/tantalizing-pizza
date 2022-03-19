a6ba27bcefc48669674c8d8d6c90d9f852d4e747
Mainafrancis <francis.maina@student.moringaschool.com>
2022-03-19 19:21:21 +0300

    functional js

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ $(document).ready(function(){
     $("#delivery").hide();
     $(".make").click(function(){
         $('#delivery').toggle();
+    });
+    $('#submit-delivery').click(function(event){
+        var person = document.getElementById("first-name").value;
+        var hood = document.getElementById("hood").value;
+        var house = document.getElementById("hseNo").value;
     })
 })
